719d4b946771b843a7907e678146a59459a92472
wyk <2226398897@qq.com>
2022-01-21 13:31:16 +0800

    增加arco-design

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,9 +2,11 @@
   <div>
     首页 {{ userStore.count }}
     <button @click="onClickAdd" class="bg-red-400">+1</button>
+    <Button type="primary">123</Button>
   </div>
 </template>
 <script setup lang="ts">
+import { Button } from '@arco-design/web-vue'
 import useUserStore from '../store/user'
 
 const userStore = useUserStore()
