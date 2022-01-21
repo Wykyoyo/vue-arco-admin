31c9631f1eb0a68b4e2a390f1c65d97000112cbb
wyk <2226398897@qq.com>
2022-01-21 10:52:08 +0800

    支持刷新token

--- src/store/user.ts
+++ src/store/user.ts
@@ -2,11 +2,17 @@ import { defineStore } from 'pinia'
 
 const useUserStore = defineStore('user', {
   state: () => ({
-    count: 0
+    count: 0,
+    token: '',
+    refreshToken: ''
   }),
   actions: {
     updateCount(count: number) {
       this.count += count
+    },
+    updateToken(token: string, refreshToken: string) {
+      this.token = token
+      this.refreshToken = refreshToken
     }
   }
 })
